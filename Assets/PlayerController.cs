3c5f2d9946bbba512beaa2e8a26e9b42230f1c43
Cptn. Dynamite Fabulous <marcus@mouyat.com>
2021-05-16 23:01:34 +1000

    Update 16/05/2021 - The start
    Created the project, with an empty scene. I made a platform and started work on a character controller. I'm not super familiar with Unity's 2D physics engine as opposed to its 3D one, but I made something that moves pleasingly left and right, when inputs are made. Jumping doesn't work for some reason, but it's not bad for roughly fifteen minutes of work.

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+[RequireComponent(typeof (Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    public float movementSpeed = 5;
+    public float jumpHeight = 5;
+
+    Vector2 movementValues;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 movementInput = new Vector2(Input.GetAxis("Horizontal") * movementSpeed, 0);
+        movementValues = movementInput * Time.fixedDeltaTime;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Debug.Log("Jump on " + Time.frameCount);
+            rb.AddForce(new Vector2(0, jumpHeight));
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 currentPosition = rb.transform.position;
+        rb.MovePosition(currentPosition + movementValues);
+    }
+}
