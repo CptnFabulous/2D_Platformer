ad509becc561a98a897cc2d2c74cb089b79411de
Cptn. Dynamite Fabulous <marcus@mouyat.com>
2021-05-24 23:05:12 +1000

    Update 24/05/2021: Attempted to make Unity's physics system actually work in the way that it's supposed to
    I attempted to make the jumping code work again, and also experimented with making a raycast check to ensure the player is grounded before being able to jump.
    
    However, this code simply does not work for some reason. Despite the if statement running when the jump button is pressed (there's a debug message that gets printed), the command that adds force simply doesn't run for some reason. Additionally, the grounded check bool simply does not run, not even displaying the console message that should run every time the bool is called, regardless of if it returns true or false. This is something I need to figure out next time, as it's late and I need to go to bed.
    
     Despite being called

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -10,7 +10,28 @@ public class PlayerController : MonoBehaviour
     Rigidbody2D rb;
 
     public float movementSpeed = 5;
+
+
+
+    [Header("Jumping")]
     public float jumpHeight = 5;
+    public float jumpCooldown = 1;
+    float jumpTimer;
+
+    public float groundedRaycastLength = 0.01f;
+
+    bool IsGrounded()
+    {
+        Debug.Log("checking");
+        if (Physics.Raycast(transform.position, -transform.up, groundedRaycastLength, ~0))
+        {
+            Debug.Log("Is grounded");
+            return true;
+        }
+
+        Debug.Log("Is not grounded");
+        return false;
+    }
 
     Vector2 movementValues;
 
@@ -29,14 +50,22 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //rb.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
+        //Debug.Log("Updating, frame " + Time.frameCount);
+        jumpTimer += Time.deltaTime;
+        if (Input.GetButtonDown("Jump")/* && jumpTimer >= jumpCooldown && IsGrounded() == true*/)
+        {
+            jumpTimer = 0;
+            Debug.Log("Jump on " + Time.frameCount);
+
+            rb.AddForce(transform.up * jumpHeight, ForceMode2D.Impulse);
+            //rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y + jumpHeight);
+        }
+
         Vector2 movementInput = new Vector2(Input.GetAxis("Horizontal") * movementSpeed, 0);
         movementValues = movementInput * Time.fixedDeltaTime;
 
-        if (Input.GetButtonDown("Jump"))
-        {
-            Debug.Log("Jump on " + Time.frameCount);
-            rb.AddForce(new Vector2(0, jumpHeight));
-        }
+        
 
     }
 
