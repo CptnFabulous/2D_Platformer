c87ec9b1212bd76b24e8acbddd1370486d4636ad
Marcus Mouyat <marcus@mouyat.com>
2022-01-05 12:51:35 +1100

    ResourceMeter base class and SegmentedMeter inherited class
    Created a base ResourceMeter class to handle different kinds of HUD meters. This meter is set up and updated by referencing a Resource struct.
    I then created an inherited class, SegmentedMeter, for resource meters consisting of a quantity of objects that are depleted one by one, such as ammunition in a gun. This consists of code to generate a series of images, and colour them based on the remaining quantity in the Resource and its critical status.

---
+++ Assets/Scripts/HUD/ResourceMeter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ResourceMeter : MonoBehaviour
+{
+    public Color filledColour;
+    public Color criticalColour;
+    public Color emptyColour;
+
+    [HideInInspector] public Resource previousValues;
+
+    //RectTransform rt;
+
+
+    public abstract void Generate(Resource values);
+    public abstract void Refresh(Resource resource);
+}
---
+++ Assets/Scripts/HUD/SegmentedMeter.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SegmentedMeter : ResourceMeter
+{
+    
+    public Image prefab;
+    public Vector3[] offsets;
+
+    Image[] segments;
+
+
+    public override void Generate(Resource values)
+    {
+        //rt = GetComponent<RectTransform>();
+
+        prefab.gameObject.SetActive(true);
+
+        Vector3 anchoredPosition = prefab.rectTransform.anchoredPosition;
+        int offsetIndex = 0;
+
+        segments = new Image[values.max];
+        for (int i = 0; i < segments.Length; i++)
+        {
+            segments[i] = Instantiate(prefab, transform);
+            segments[i].rectTransform.anchoredPosition = anchoredPosition;
+
+            anchoredPosition += offsets[offsetIndex];
+            offsetIndex++;
+            if (offsetIndex >= offsets.Length)
+            {
+                offsetIndex = 0;
+            }
+        }
+        prefab.gameObject.SetActive(false);
+        Refresh(values);
+    }
+
+    public override void Refresh(Resource resource)
+    {
+        Color fillColour = resource.critical ? criticalColour : filledColour;
+        for (int i = 0; i < segments.Length; i++)
+        {
+            
+            segments[i].color = (i + 1 <= resource.current) ? fillColour : emptyColour;
+        }
+        previousValues = resource;
+        /*
+        // Counts up only through the values making up the difference between the old and new value. The other icons don't need to change.
+        int min = Mathf.Max(1, previousValues.current);
+        if (resource.critical == previousValues.critical)
+        {
+            min = Mathf.Min(min, resource.current);
+        }
+        int max = Mathf.Max(previousValues.current, resource.current);
+        Color fillColour = resource.critical ? criticalColour : filledColour;
+        for (int i = min; i < max; i++)
+        {
+            Debug.Log(i - 1);
+            segments[i - 1].color = (i <= resource.current) ? fillColour : emptyColour;
+        }
+        previousValues = resource;
+        */
+    }
+
+    
+    
+}
