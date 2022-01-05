9c4060a91c59e7f91b949e041dde4a410f5e5190
Marcus Mouyat <marcus@mouyat.com>
2022-01-05 11:12:45 +1100

    Aim controller now uses mouse cursor rather than rotating on axis
    I realised the old aiming code (which used two keyboard buttons to rotate the aim axis) was incredibly awkward and uncomfortable to use. I reworked it to now use the mouse cursor, so the player will aim in the direction of whatever the mouse is hovering over. Simple and effective!

--- Assets/AimController.cs
+++ Assets/AimController.cs
@@ -4,29 +4,65 @@ using UnityEngine;
 
 public class AimController : MonoBehaviour
 {
-    public float sensitivity = 120f;
-    public Transform aimOrigin;
-    float aimAngle = 0;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform weaponAxis;
+    public Transform weaponHUDSocket;
+    public Camera playerCamera;
 
-    // Update is called once per frame
-    void Update()
-    {
-        aimAngle -= Input.GetAxis("RotateAim") * sensitivity * Time.deltaTime;
-        Debug.DrawRay(aimOrigin.position, AimDirection * 5, Color.red);
-    }
 
-    public Vector2 AimDirection
+    public Vector2 aimDirection { get; private set; }
+    public Vector3 aimDirection3D
     {
         get
+        {
+            return new Vector3(aimDirection.x, aimDirection.y, 0);
+        }
+    }
+    public float AimAngle
+    {
+        get
+        {
+            return Vector2.SignedAngle(Vector2.up, aimDirection) + 90;
+        }
+    }
+    
+
+    
+
+
+    void Update()
+    {
+        
+        
+        aimDirection = (Input.mousePosition - playerCamera.WorldToScreenPoint(weaponAxis.position)).normalized;
+        //Debug.DrawRay(weaponAxis.position, aimDirection * 5, Color.cyan);
+        //Debug.DrawRay(weaponAxis.position, AngleToAimDirection(AimAngle) * 5, Color.blue);
+
+
+
+
+        //aimAngle -= Input.GetAxis("RotateAim") * sensitivity * Time.deltaTime;
+        
+        weaponAxis.LookAt(weaponAxis.position + aimDirection3D, transform.up);
+
         {
             float aimRadian = aimAngle * Mathf.Deg2Rad;
             return new Vector2(Mathf.Cos(aimRadian), Mathf.Sin(aimRadian));
         }
+
+
+        //Debug.Log(direction + ", " + AimDirection(aimAngle));
     }
+
+    
+    public Vector2 AngleToAimDirection(float angle)
+    {
+        float aimRadian = angle * Mathf.Deg2Rad;
+        return new Vector2(Mathf.Cos(aimRadian), Mathf.Sin(aimRadian));
+    }
+    public Vector3 AngleToAimDirection3D(float angle)
+    {
+        float aimRadian = angle * Mathf.Deg2Rad;
+        return new Vector3(Mathf.Cos(aimRadian), Mathf.Sin(aimRadian), 0);
+    }
+    
 }
