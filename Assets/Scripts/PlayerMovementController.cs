25795103f499c305b9183850ba33c051ccc1e124
Marcus Mouyat <marcus@mouyat.com>
2022-04-29 14:29:26 +1000

    New player movement controller
    I have began work on a new player controller, that feels much better than the previous one.
    Changes:
    •	Uses new Unity Input System package
    •	Tighter movement and changing of direction
    •	Jumps feel less floaty
    New features:
    •	Coyote time – jump will occur even if the player presses the button just after leaving solid ground. This increased input window makes fine control a lot easier and improves general responsiveness.
    •	Jump buffer – jump will occur even if the player presses the button just before landing on solid ground. Like with coyote time, this increases the acceptable input window, for more responsive controls.
    •	Apex float – player will float for a short time after reaching the apex of their jump (i.e. once their vertical velocity is no longer increasing). This makes air control slightly easier.
    There’s still much in the way of new features to add, but it already feels much better than the old controller!

---
+++ Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,144 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovementController : MonoBehaviour
+{
+    /*
+    Inspirations
+    https://www.youtube.com/watch?v=vAZV5xO_AHU
+    https://www.youtube.com/watch?v=3sWTzMsmdx8
+    */
+
+    [Header("Movement")]
+    [Min(0)] public float movementSpeed = 5;
+
+    [Header("Grounding")]
+    public LayerMask terrainDetection = ~0;
+    [Min(0)] public float groundingCheckLength = 1.1f;
+    [Min(0)] public float wallCheckLength = 0.6f;
+    [Range(0, 90)] public float groundAngle = 45;
+
+    [Header("Jumping")]
+    [Min(0)] public float jumpForce = 5;
+    [Min(0)] public float jumpCooldown = 0.5f;
+    [Min(0)] public float coyoteTime = 0.1f;
+    [Min(0)] public float jumpBufferTime = 0.1f;
+    [Min(0)] public float jumpApexTime = 0.1f;
+
+    [Header("Gravity")]
+    public float gravityScale = 2;
+    [Min(0)] public float maxFallSpeed = 10;
+
+    Rigidbody2D rb;
+    CapsuleCollider2D collider;
+    Vector2 movementValues; // The current movement input
+
+    public RaycastHit2D groundData { get; private set; } // The thing the player is currently standing on
+    float lastTimeGrounded;
+
+    float lastTimeJumpAttempted;
+    float lastTimeJumped;
+    float lastTimeAscending;
+
+
+    #region Properties
+    /// <summary>
+    /// Returns the player's velocity, but relative to their rotation. Also used to directly set velocity based on local values
+    /// </summary>
+    public Vector2 localVelocity
+    {
+        get => Quaternion.Inverse(transform.rotation) * rb.velocity;
+        set => rb.velocity = transform.rotation * value;
+    }
+    /// <summary>
+    /// Collider size in world space.
+    /// </summary>
+    public Vector2 worldColliderSize => collider.size * transform.lossyScale;
+    public bool isGrounded
+    {
+        get
+        {
+            if (groundData.collider == null) return false;
+            if (Vector2.Angle(-groundData.normal, Physics.gravity) > groundAngle) return false;
+            return true;
+        }
+    }
+    #endregion
+
+    #region Input functions
+    public void OnMove(InputValue input)
+    {
+        movementValues = input.Get<Vector2>();
+    }
+    public void OnJump(InputValue input)
+    {
+        if (input.isPressed)
+        {
+            lastTimeJumpAttempted = Time.time;
+        }
+    }
+    #endregion
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        collider = GetComponent<CapsuleCollider2D>();
+    }
+    private void FixedUpdate()
+    {
+        CheckGrounding();
+        CheckJump();
+        CheckJumpApex();
+
+        Vector2 newVelocity = localVelocity;
+
+        newVelocity.x = movementValues.x * movementSpeed; // Set velocity to movement direction and speed
+
+
+        localVelocity = newVelocity; // Set vertical velocity
+    }
+
+    void CheckGrounding()
+    {
+        Vector2 box = worldColliderSize;
+        box.y *= 0.1f;
+        groundData = Physics2D.BoxCast(collider.bounds.center, box, transform.eulerAngles.z, -transform.up, groundingCheckLength, terrainDetection);
+
+        if (isGrounded)
+        {
+            lastTimeGrounded = Time.time;
+        }
+    }
+    void CheckJump()
+    {
+        if (Time.time - lastTimeJumpAttempted > jumpBufferTime) return; // Is the buffer still active since the player pressed the jump button?
+        if (Time.time - lastTimeGrounded > coyoteTime) return; // Is the player grounded (or is coyote time still in effect?)
+        if (Time.time - lastTimeJumped < jumpCooldown) return; // Has the cooldown elapsed?
+
+        lastTimeJumped = Time.time; // Reset the jump-related variables
+
+        // Jump!
+        localVelocity = new Vector2(localVelocity.x, jumpForce);
+    }
+    void CheckJumpApex()
+    {
+        if (isGrounded)
+        {
+            // If the player is not airborne, set normal gravity scale and don't perform any other calculations
+            rb.gravityScale = gravityScale;
+            return;
+        }
+
+        if (localVelocity.y > 0) // if the player is ascending, log time. 
+        {
+            lastTimeAscending = Time.fixedTime;
+        }
+        else // When they aren't, the time is not updated and represents the last time they were.
+        {
+            bool inApexTime = Time.fixedTime - lastTimeAscending < jumpApexTime;
+            rb.gravityScale = inApexTime ? 0 : gravityScale;
+        }
+    }
+}
