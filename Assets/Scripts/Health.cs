39f8f400eb8e8b292559f955bf22da03a61bf150
Marcus Mouyat <marcus@mouyat.com>
2022-05-17 08:32:18 +1000

    Health class
    This MonoBehaviour stores health data for any object that can take damage and be destroyed. Currently there isn't any damage code that I plan on using, but I know I will implement some later.

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Health : MonoBehaviour
+{
+    public Resource data;
+
+    public UnityEvent<Resource> onDamage;
+    public UnityEvent<Resource> onHeal;
+    public UnityEvent onDeath;
+
+    public void Damage(int amount)
+    {
+        if (data.depleted)
+        {
+            return;
+        }
+
+        data.Increment(-amount);
+        if (amount < 0)
+        {
+            onHeal.Invoke(data);
+        }
+        else
+        {
+            onDamage.Invoke(data);
+        }
+        
+        if (data.depleted)
+        {
+            onDeath.Invoke();
+        }
+    }
+}
